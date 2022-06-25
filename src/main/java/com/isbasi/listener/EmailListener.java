f2cc1651a3d02a1f034193993541b3b251050feb
cem dırman <cem.dirman@cems-Mac-mini.local>
2022-06-25 22:59:46 +0300

    modified listener object

--- src/main/java/com/isbasi/listener/EmailListener.java
+++ src/main/java/com/isbasi/listener/EmailListener.java
@@ -3,15 +3,17 @@ package com.isbasi.listener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import com.isbasi.dto.EmailDto;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class EmailListener {
-	
+
 	@RabbitListener(queues = "isbasi.email")
-	public void emailListener(String email) {
-		log.info("email address: {}", email);	
+	public void emailListener(EmailDto emailDto) {
+		log.info("email address: {}", emailDto.getEmail());
 		// TO DO mail at
 	}
 
